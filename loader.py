98c3a9b3e098950e09a37a7510c7580ed537312b
strayge <stray.omsk@gmail.com>
2015-12-15 18:41:58 +0600

    refactoring

--- loader.py
+++ loader.py
@@ -8,29 +8,20 @@ import socket
 from html.parser import unescape
 from multiprocessing import Queue, freeze_support, Process, current_process
 import queue
-
 import socks
 import requests
-
-
-
-# ############# SETTINGS ###############
-# Rutracker.org login & pass
 import time
 
 default_login = 'LOGIN'
 default_password = 'PASSWORD'
 default_proxy_port = 9150
 default_threads_num = 1
-######################################
 default_ids_file = 'ids.txt'
 table_file = "table.txt"
 default_html_folder = 'html'
 default_descr_folder = 'descr'
 default_proxy_file = 'proxy.txt'
 default_login_file = 'login.txt'
-domain, path_tmpl = 'rutracker.org', '/forum/viewtopic.php?t=%(id)i'
-
 
 class SomeError(Exception):
     def __init__(self, value, fatal=True):
@@ -45,13 +36,12 @@ def get_rutracker_cookie(login, password):
     post_params = {
         'login_username': login,
         'login_password': password,
-        'login': b'\xc2\xf5\xee\xe4'  # '%C2%F5%EE%E4'
+        'login': b'\xe2\xf5\xee\xe4'  # '%E2%F5%EE%E4'
     }
-    # time.sleep(random.randrange(2, 5))  # DEBUG
-    r = requests.post('http://login.rutracker.org/forum/login.php', data=post_params)
+    r = requests.post('http://login.rutracker.org/forum/login.php', data=post_params, allow_redirects=False, timeout=(6, 27))
     if 'bb_data' in r.cookies.keys():
         cookie = 'bb_data=' + r.cookies['bb_data'] + '; tr_simple=1; spylog_test=1'
-        # print('DEBUG', 'cookie =', cookie)  # DEBUG
+        print('DEBUG', 'cookie =', cookie)  # DEBUG
         return cookie
     else:
         raise SomeError('no cookies, get_rutracker_cookie')
@@ -93,19 +83,7 @@ def parse_rutracker(id, html):
             title = between(text, '<title>', ' :: RuTracker.org')
             title = unescape(title)
             line.append(title)
-            size_parts = between(text, '<td id="tor-size-humn">', '</td>')
-            size_parts = size_parts.split('&nbsp;')
-            # size_parts = between(text, 'Размер:&nbsp; <b>', '</b>').split('&nbsp;')
-            size = float(size_parts[0])
-            if size_parts[1] == 'KB':
-                size *= 1024
-            if size_parts[1] == 'MB':
-                size *= 1024 ** 2
-            if size_parts[1] == 'GB':
-                size *= 1024 ** 3
-            if size_parts[1] == 'TB':
-                size *= 1024 ** 4
-            size = str(int(size))
+            size = between(text, '<span id="tor-size-humn"', '</span>')
             line.append(size)
             seeds = '0'
             if '<span class="seed">Сиды:&nbsp; <b>' in text:
@@ -119,25 +97,26 @@ def parse_rutracker(id, html):
             line.append(hash)
             if 'torrent скачан:&nbsp; <b>' in text:
                 downloads = between(text, 'torrent скачан:&nbsp; <b>', " раз")
-            else:
+            elif '<td>.torrent скачан:</td>\n\t\t<td>' in text:
                 downloads = between(text, '<td>.torrent скачан:</td>\n\t\t<td>', " раз")
+            else:
+                downloads = between(text, 'Скачан: ', 'раз\t\t</td>')
             line.append(downloads)
             if 'Зарегистрирован &nbsp;[ ' in text:
                 date = between(text, 'Зарегистрирован &nbsp;[ ', ' ]')
             else:
                 date = between(text, 'зарегистрирован">[ ', ' ]<')
-            # date = between(text, 'зарегистрирован">[ ', ' ]<')
             date = fix_date(date)
             line.append(date)
 
             category_htmlpart = between(text, '<td class="nav w100"', '</td>')
             category_temp = category_htmlpart.replace('">', "</a>").split('</a>')
-            category_list = list((i for i in category_temp if ('<em>' not in i) and ('\t' not in i) and ('style="' not in i) and ('Список форумов ' not in i)))
+            category_list = list((i for i in category_temp if
+                                  ('<em>' not in i) and ('\t' not in i) and ('style="' not in i) and ('Список форумов ' not in i)))
             category_string = ''
             for one_category in category_list:
                 category_string += one_category + ' | '
             category_string = category_string[:-3]
-            # print(category_string)
             line.append(category_string)
 
             line = '\t'.join(line)
@@ -154,12 +133,14 @@ def parse_rutracker(id, html):
         raise SomeError('no login', fatal=False)
     if len(text) < 1000:
         raise SomeError('too short', fatal=False)
+    f = open('html.txt', "w")
+    f.write(text)
     return do_with(id, text)
 
 
 def process_rutracker_page(id, headers):
-    path = path_tmpl % {'id': id}
-    url = 'http://%(domain)s%(path)s' % {'domain': domain, 'path': path}
+    path = '/forum/viewtopic.php?t=%(id)i' % {'id': id}
+    url = 'http://rutracker.org%(path)s' % {'path': path}
     req = requests.get(url, headers=headers)
     html = req.text
     if not (('<html>' in html) or ('<HTML>' in html)):
@@ -177,38 +158,28 @@ def worker(input, output, cookie, proxy, options):
         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", proxy)
         socket.socket = socks.socksocket
 
-    # try:
-    #     cookie = get_rutracker_cookie(login, password)
-    # except requests.exceptions.RequestException:
-    #     output.put((current_process().name, 'ERROR', 'RequestException in get_rutracker_cookie'))
-    #     exit()
-    # except SomeError as e:
-    #     output.put((current_process().name, 'ERROR', e.value))
-    #     exit()
-
     useragents = ['Mozilla/5.0 (Android; Mobile; rv:38.0) Gecko/38.0 Firefox/38.0',
                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3',
-                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
-                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
                   'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
                   'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43/0/2357/81 Safari/537.36',
                   'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
                   'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
                   'Mozilla/5.0 (X11; Ubunru; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0',
                   'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/7.1.5 Safari/537.85.15'
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/7.1.5 Safari/537.85.15',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0',
+                  'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
                   ]
 
     headers = {
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Host': domain,
+        'Accept-Encoding': 'gzip,deflate',
+        'Host': 'rutracker.org',
         'Accept-Language': 'ru,en-US;q=0.8,en;q=0.6',
         'User-Agent': useragents[random.randrange(0, len(useragents))],
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Referer': 'http://%s/' % domain,
+        'Referer': 'http://rutracker.org/forum/index.php',
         'Cookie': cookie,
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0'
+        'Connection': 'keep-alive'
     }
 
     for id in iter(input.get, 'STOP'):
@@ -232,6 +203,7 @@ if __name__ == '__main__':
     ids_group = ap.add_mutually_exclusive_group(required=True)
     ids_group.add_argument('--ids_file', '-if')
     ids_group.add_argument('--ids', nargs=2, type=int)
+    ap.add_argument('--old', '-old')
     ap.add_argument('--noproxy', '--direct', '-d', action="store_true")
     ap.add_argument('--port', '-p')
     ap.add_argument('--user', '-u')
@@ -239,11 +211,12 @@ if __name__ == '__main__':
     ap.add_argument('--html')
     ap.add_argument('--folder', '-f')
     ap.add_argument('--cookie')
+    ap.add_argument('--cookies_list')
     ap.add_argument('--random', action="store_true")
     ap.add_argument('--threads', '-tr', type=int)
     ap.add_argument('--proxy_file', '-pf', '-pr')
     ap.add_argument('--login_file', '-lf')
-    ap.add_argument('--restore', action="store_true")
+    ap.add_argument('--restore', '--resume', action="store_true")
     ap.add_argument('--print', action="store_true")
     options = ap.parse_args()
 
@@ -281,41 +254,46 @@ if __name__ == '__main__':
 
     cookie_proxy_list = []
 
-    last_i = 0
-    for i in range(len(login_list)):
-        last_i = i
-        l, p = login_list[i]
-        proxy = int(proxy_list[i % len(proxy_list)])
-        if options.noproxy:
-            # print('no using proxy')
-            pass
-        else:
-            # print(('using proxy, port ' + str(proxy)))
-            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", proxy)
-            socket.socket = socks.socksocket
+    if not options.cookies_list:
+        last_i = 0
+        for i in range(len(login_list)):
+            last_i = i
+            l, p = login_list[i]
+            proxy = int(proxy_list[i % len(proxy_list)])
+            if options.noproxy:
+                # print('no using proxy')
+                pass
+            else:
+                # print(('using proxy, port ' + str(proxy)))
+                socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", proxy)
+                socket.socket = socks.socksocket
 
-        try:
-            cookie = get_rutracker_cookie(l, p)
-            cookie_proxy_list.append([cookie, proxy])
-            print('OK, proxy: ' + str(proxy) + ', login: ' + l)
-        except:
-            print('ERROR, proxy: ' + str(proxy) + ', login: ' + l)
-        time.sleep(0.7)
+            try:
+                cookie = get_rutracker_cookie(l, p)
+                cookie_proxy_list.append([cookie, proxy])
+                print('OK, proxy: ' + str(proxy) + ', login: ' + l)
+            except:
+                print('ERROR, proxy: ' + str(proxy) + ', login: ' + l)
+            time.sleep(0.7)
 
-        if len(cookie_proxy_list) >= threads_num:
-            print('stop cookies, break')
-            break
-        print('cookie_proxy_list length: ' + str(len(cookie_proxy_list)))
+            if len(cookie_proxy_list) >= threads_num:
+                print('stop cookies, break')
+                break
+            print('cookie_proxy_list length: ' + str(len(cookie_proxy_list)))
 
-    max_threads = min(len(cookie_proxy_list) * 2, threads_num)
-    print('max threads =', max_threads)
-    for j in range(max_threads - len(cookie_proxy_list)):
-        proxy = int(proxy_list[(last_i + j) % len(proxy_list)])
-        cookie_proxy_list.append([cookie_proxy_list[j][0], proxy])
+        max_threads = min(len(cookie_proxy_list) * 2, threads_num)
+        print('max threads =', max_threads)
+        for j in range(max_threads - len(cookie_proxy_list)):
+            proxy = int(proxy_list[(last_i + j) % len(proxy_list)])
+            cookie_proxy_list.append([cookie_proxy_list[j][0], proxy])
+    else:
+        f = open(options.cookies_list)
+        for line in f:
+            c, p = line.split(', ')
+            cookie_proxy_list.append([c, int(p)])
 
-    print(cookie_proxy_list)
     processes = list()
-    for i in range(len(cookie_proxy_list)):
+    for i in range(min(threads_num, len(cookie_proxy_list))):
         login, password = login_list[i % len(login_list)]
         cookie, proxy = cookie_proxy_list[i % len(cookie_proxy_list)]
         # proxy = int(proxy_list[i % len(proxy_list)])
@@ -324,11 +302,22 @@ if __name__ == '__main__':
         p.start()
         processes.append(p)
 
-    # ==============================================
     if options.ids_file:
         ids = set(map(int, open(ids_file)))
     else:
         ids = set(range(options.ids[0], options.ids[1]))
+
+    if options.old:
+        max_id = 0
+        #ignoring blank lines
+        old_ids = set(map(int, filter(lambda s: s != '', map(lambda s: s.strip(), open(options.old)))))
+        # old_ids = set(map(int, open(options.old)))
+        for id in old_ids:
+            if id > max_id:
+                max_id = id
+        for id in range(1, max_id):
+            if (id not in old_ids) and (id in ids):
+                ids.remove(id)
 
     if options.restore:
         ids_finished = set(map(int, open('finished.txt')))
@@ -349,11 +338,8 @@ if __name__ == '__main__':
         print('Empty input/left list. Terminated.')
         exit()
 
-    # print('DEBUG', ids)  # DEBUG
-
     if options.random:
         random.shuffle(ids)
-    # ==============================================
 
     for number, id in enumerate(ids):
         id = int(id)
@@ -370,7 +356,6 @@ if __name__ == '__main__':
     finished_file = open('finished.txt', 'a', encoding='utf8')
     log_file = open('log.txt', 'a', encoding='utf8')
 
-    # for i in range(len(ids)):
     while True:
         if task_queue.empty():
             print('Task queue is empty.')
